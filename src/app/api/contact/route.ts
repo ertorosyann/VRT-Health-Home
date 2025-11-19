4eb680802ac39d0f3b557ab74f68370c596ed550
ertorosyann <ertorosyann@gmail.com>
2025-11-19 14:26:48 +0400

    update email sender addres to my

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -211,6 +211,7 @@ export async function POST(request: NextRequest) {
       replyTo: sanitizedData.email || emailUser, // Replies will go to user's email
       // to: 'vrtcare@gmail.com',
       to: 'ertorosyann@gmail.com',
+      
       subject: `New Contact Form Submission from ${sanitizedData.firstName} ${sanitizedData.lastName}`,
       html: `
         <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto;">
